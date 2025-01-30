66d8c17d1619909c541fe23d815cc0bf68f97be7
Mehmet <mgsert@yahoo.ca>
2025-01-30 21:18:56 +0300

    First

--- src/Connect4.java
+++ src/Connect4.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
 public class Connect4 {
+    static final int ROWS=6;
+    static final int COLS=7;
+    static int [][] board = new int[ROWS][COLS];
+    static int currentPlayer = 1 ;
+
     
 }
