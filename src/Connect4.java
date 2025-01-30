55b9fdd9a3624ccdd7d21f8f72d81fdb2c723af2
Mehmet <mgsert@yahoo.ca>
2025-01-30 21:27:11 +0300

    Second

--- src/Connect4.java
+++ src/Connect4.java
@@ -5,5 +5,23 @@ public class Connect4 {
     static int [][] board = new int[ROWS][COLS];
     static int currentPlayer = 1 ;
 
-    
+    public static void main (String[]args) {
+        Scanner scanner = new Scanner(System.in);
+        initializeBoard();
+
+        while (true) {
+            printBoard();
+            System.out.println("Player "+ currentPlayer + " 's turn. Choose a column  (0-6): ");
+            int column =scanner.nextInt();
+
+            if (column<0 || column >=COLS) {
+                System.out.println(" Invalid column.Please choose a column between 0 and 6, ");
+                continue;
+            }
+
+            
+        }
+    }
+
+
 }
